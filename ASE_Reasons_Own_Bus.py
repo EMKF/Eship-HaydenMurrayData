ac0bb36350ccdd03356b0ddc159f7428a18ddff3
Hayden Murray <hmurray@MacMurH111919.kauffman.org>
2020-02-19 13:20:42 -0600

    ASE

--- ASE_Reasons_Own_Bus.py
+++ ASE_Reasons_Own_Bus.py
@@ -12,11 +12,8 @@ pd.set_option('display.max_rows', 30000)
 pd.set_option('max_colwidth', 4000)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
-df = pd.read_csv('/Users/hmurray/Desktop/Data_Briefs/ASE/Maddi_Oldstone/MO_Data_Briefs/ASE_Reasons_Own_Business/ASE_Reasons_Own_Bus.csv'
-                 ,header=0,encoding = 'unicode_escape', dtype={'user_id': int}, low_memory=False)
-
-# condense dataframe
-df = df[['GEO.display-label','ASECBO.display-label','YIBSZFI.display-label','REASONOWN.display-label','OWNPDEMP_PCT']]
+def data_in()
+df = pd.read_csv('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/ASE_2016_reasons_own_business.csv')
 print(df.head())
 
 
