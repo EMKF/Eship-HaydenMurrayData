d8d8e7ec899a2df49e4e57afa698a73fc92e9c57
Hayden Murray <hmurray@MacMurH111919.local>
2020-02-21 07:42:23 -0600

    own bus ASE

--- ASE_Reasons_Own_Bus.py
+++ ASE_Reasons_Own_Bus.py
@@ -22,7 +22,6 @@ df = df[['GEO.display-label', 'NAICS.display-label', 'ASECBO.display-label', 'YI
 # Rename columns
 df.rename(columns={"GEO.display-label": "region", "NAICS.display-label": "industry", "ASECBO.display-label": "demographic"\
     ,"YIBSZFI.display-label": "firm_age", "REASONOWN.display-label": "reason"},inplace=True)
-print(df.head())
 
 # observation filter: all
 df_all = df[df['region'] == 'United States']
@@ -36,7 +35,7 @@ print(df_all)
 # observation filter: gender
 df_gender = df[df['region'] == 'United States']
 df_gender = df_gender[df_gender['industry'] == 'Total for all sectors']
-df_gender = [df_gender['demographic'] == 'Male' or 'Female']
+df_gender = df_gender[(df_gender.demographic == 'Male') | (df_gender.demographic == 'Female')]
 df_gender = df_gender[df_gender['firm_age'] == 'All firms']
 df_gender.reset_index(inplace=True, drop=True)
 df_gender.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/gender_reasons_own_bus.xlsx', index=False)
@@ -45,27 +44,10 @@ print(df_gender)
 # observation filter: race
 df_race = df[df['region'] == 'United States']
 df_race = df_race[df_race['industry'] == 'Total for all sectors']
-df_race = df_race[df_race['demographic'] == 'Hispanic' or 'White' or 'Asian' or 'Black or African American']
+df_race = df_race[(df_race.demographic == 'Hispanic') | (df_race.demographic == 'White') | (df_race.demographic == 'Asian') | (df_race.demographic == 'Black or African American')]
 df_race = df_race[df_race['firm_age'] == 'All firms']
 df_race.reset_index(inplace=True, drop=True)
 df_race.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/race_reasons_own_bus.xlsx', index=False)
 print(df_race)
 
-# df_gender =
-# df_race =
-    # if demographic == 'all':
-    #     df = df.loc[df['ASECBO.display-label'] == "All owners of respondent firms"]
-    # elif demographic == 'gender':
-    #     df = df.loc[(df['ASECBO.display-label'] == "Male") | (df['ASECBO.display-label'] == "Female")]
-    # else:  # race
-    #     df = df.loc[
-    #         (df['ASECBO.display-label'] == "Hispanic") |
-    #         (df['ASECBO.display-label'] == "White") |
-    #         (df['ASECBO.display-label'] == "Asian") |
-    #         (df['ASECBO.display-label'] == "Black or African American")
-    #     ]
-    # return df.\
-    #     loc[df['YIBSZFI.display-label'] == "All firms"].\
-    #     loc[df['NAICS.display-label'] == "Total for all sectors"].\
-    #     query('OWNPDEMP_PCT == OWNPDEMP_PCT').\
-    #     reset_index(drop=True)
+sys.exit()
