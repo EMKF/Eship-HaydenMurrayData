40f4e84218d3e0ed5de678c7b0532e08b934d89a
Hayden Murray <hmurray@MacMurH111919.local>
2020-02-24 11:33:14 -0600

    ASE

--- ASE_Reasons_Own_Bus.py
+++ ASE_Reasons_Own_Bus.py
@@ -14,8 +14,31 @@ pd.set_option('display.max_rows', 30000)
 pd.set_option('max_colwidth', 4000)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
+
+# KESE - OPPORTUNITY SHARE OF ENTREPRENEURS
+kese = pd.read_csv('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/original_pull/KESE_opp.csv')
+
+# United States OSE
+kese_us = kese[51:52]
+kese_us = kese_us.iloc[:,:26]
+kese_us.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/kese_opp_US.xlsx', index=False)
+print(kese_us)
+
+# Gender OSE
+kese_gender = kese[(kese.demographic == 'Men') | (kese.demographic == 'Women')]
+kese_gender = kese_gender.iloc[:,:26]
+kese_us.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/kese_opp_gender.xlsx', index=False)
+print(kese_gender)
+
+# Race OSE
+kese_race = kese[(kese.demographic == 'White') | (kese.demographic == 'Black')| (kese.demographic == 'Latino') | (kese.demographic == 'Asian')]
+kese_race = kese_race.iloc[:,:26]
+kese_us.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/kese_opp_race.xlsx', index=False)
+print(kese_race)
+
+# ASE - REASONS WHY ENTREPRENEURS OWN THEIR BUSINESS
 # import data
-df = pd.read_csv('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/ASE_2016_reasons_own_business.csv')
+df = pd.read_csv('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/original_pull/ASE_2016_reasons_own_business.csv')
 
 # keep vars
 df = df[['GEO.display-label', 'NAICS.display-label', 'ASECBO.display-label', 'YIBSZFI.display-label', 'REASONOWN.display-label', 'OWNPDEMP_PCT']]
@@ -52,9 +75,22 @@ print(df_gender)
 df_race = df[df['region'] == 'United States']
 df_race = df_race[df_race['industry'] == 'Total for all sectors']
 df_race = df_race[(df_race.demographic == 'Hispanic') | (df_race.demographic == 'White') | (df_race.demographic == 'Asian') | (df_race.demographic == 'Black or African American')]
-df_race = df_race[df_race['firm_age'] == 'All firms']
+# df_race = df_race[df_race['firm_age'] == 'All firms']
 df_race.reset_index(inplace=True, drop=True)
 df_race.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/race_reasons_own_bus.xlsx', index=False)
 print(df_race)
 
-sys.exit()
+# observation filter: race very important
+df_all_very = df_all[df_all['reason'].str.contains("Very")]
+df_all_very.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/very_all_reasons_own_bus.xlsx', index=False)
+
+# observation filter: gender very important
+df_gender_very = df_gender[df_gender['reason'].str.contains("Very")]
+df_gender_very.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/very_gender_reasons_own_bus.xlsx', index=False)
+
+# observation filter: race very important
+df_race_very = df_race[df_race['reason'].str.contains("Very")]
+df_race_very.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/very_race_reasons_own_bus.xlsx', index=False)
+
+
+
