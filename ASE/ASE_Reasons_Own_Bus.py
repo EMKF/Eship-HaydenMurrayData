89352da7fe81f6bd7293843d5ce5cdb28bfb6280
Hayden Murray <hmurray@MacMurH111919.local>
2020-03-03 07:19:58 -0600

    Why start a business

--- ASE/ASE_Reasons_Own_Bus.py
+++ ASE/ASE_Reasons_Own_Bus.py
@@ -14,6 +14,16 @@ pd.set_option('display.max_rows', 30000)
 pd.set_option('max_colwidth', 4000)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
+# KESE - Rate of New Entrepreneurs output
+rne = pd.read_csv('/Users/hmurray/Desktop/KESE/KESE_2018_data/KESE_rate.csv')
+rne = rne[52:54]
+rne.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/rne_gender.xlsx', index=False)
+
+# NEB - Rate of New Employer Businesses output
+rneb = pd.read_excel('/Users/hmurray/Desktop/NEB/NEB_Data/FINAL_DATA/NEB_Rate.xlsx')
+rneb = rneb[51:52]
+print(rneb)
+rneb.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/rneb_us.xlsx', index=False)
 
 # KESE - OPPORTUNITY SHARE OF ENTREPRENEURS
 kese = pd.read_csv('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/original_pull/KESE_opp.csv')
@@ -33,8 +43,15 @@ print(kese_gender)
 # Race OSE
 kese_race = kese[(kese.demographic == 'White') | (kese.demographic == 'Black')| (kese.demographic == 'Latino') | (kese.demographic == 'Asian')]
 kese_race = kese_race.iloc[:,:26]
-kese_race.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/kese_opp_race.xlsx', index=False)
+
+# standard deviations for OSE
+kese_race['std'] = kese_race.std(axis=1)
+kese_race['max'] = kese_race.max(axis=1)
+kese_race['min'] = kese_race.loc[:, ['ose-1998', 'ose-1999', 'ose-2000', 'ose-2001', 'ose-2002', 'ose-2003', 'ose-2004', 'ose-2005',\
+                                     'ose-2006', 'ose-2007', 'ose-2008', 'ose-2009', 'ose-2010', 'ose-2011', 'ose-2012', 'ose-2013',\
+                                     'ose-2014', 'ose-2015', 'ose-2016', 'ose-2017', 'ose-2018']].min(axis=1)
 print(kese_race)
+kese_race.to_excel('/Users/hmurray/Desktop/Data_Briefs/ASE/MO_ASE_Reasons_Own_Business/Data_Reasons_Own_Business/kese_opp_race.xlsx', index=False)
 
 # ASE - REASONS WHY ENTREPRENEURS OWN THEIR BUSINESS
 # import data
