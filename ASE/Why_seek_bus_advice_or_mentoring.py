77190cf3910d26b73587e7e553134dea01bb6755
Hayden Murray <hmurray@MacMurH111919.local>
2020-03-06 10:25:15 -0600

    advice

--- ASE/Why_seek_bus_advice_or_mentoring.py
+++ ASE/Why_seek_bus_advice_or_mentoring.py
@@ -28,7 +28,15 @@ reasons.rename(
     columns={"GEO.display-label": "region", "NAICS.display-label": "industry", "ASECB.display-label": "demographic" \
         , "YIBSZFI.display-label": "firm_age", "SEEKADV.display-label": "reason", "FIRMPDEMP_PCT": "percent"},
     inplace=True)
-reasons = reasons[(reasons.reason != 'Other') | (reasons.reason != 'Total reporting') | (reasons.reason != 'Item not reported')]
+
+reasons = reasons[reasons.reason != 'All firms']
+reasons = reasons[reasons.reason != 'Other']
+reasons = reasons[reasons.reason != 'Total reporting']
+reasons = reasons[reasons.reason != 'Item not reported']
+reasons = reasons.drop(reasons.index[0:1])
+reasons.reset_index(inplace=True, drop=True)
+
+
 
 
 # SOURCE OF BUSINESS ADVICE OR MENTORING
@@ -41,7 +49,10 @@ source.rename(
     columns={"GEO.display-label": "region", "NAICS.display-label": "industry", "ASECB.display-label": "demographic" \
         , "YIBSZFI.display-label": "firm_age", "PROVADV.display-label": "source", "FIRMPDEMP_PCT": "percent"},
     inplace=True)
-source = source[(source.source != 'Item not reported') | (source.source != 'Not applicable') | (reasons.reason != 'All firms')]
+source = source[source.source != 'Item not reported']
+source = source[source.source != 'Not applicable']
+source = source[source.source != 'All firms']
+
 
 
 # OUTCOME OF BUSINESS ADVICE OR MENTORING
@@ -54,11 +65,15 @@ outcome.rename(
     columns={"GEO.display-label": "region", "NAICS.display-label": "industry", "ASECB.display-label": "demographic" \
         , "YIBSZFI.display-label": "firm_age", "ADVOUTCOME.display-label": "outcome", "FIRMPDEMP_PCT": "percent"},
     inplace=True)
-outcome = outcome[(outcome.outcome != 'Item not reported') | (outcome.outcome != 'Not applicable') | (outcome.outcome != 'All firms')]
+outcome = outcome[outcome.outcome != 'Item not reported']
+outcome = outcome[outcome.outcome != 'Not applicable']
+outcome = outcome[outcome.outcome != 'All firms']
+outcome = outcome[outcome.outcome != 'Total reporting']
+
 
 
 # FILTER FUNCTION
-def filterer(df, save=None):
+def filterer(df):
     df = df[df['region'] == 'United States']
     df = df[df['industry'] == 'Total for all sectors']
     df = df[(df.demographic == 'All firms') | (df.demographic == 'Male')\
@@ -68,11 +83,22 @@ def filterer(df, save=None):
     df.reset_index(inplace=True, drop=True)
     df.drop(df.columns[0:2], axis=1, inplace=True)
     df.reset_index(inplace=True, drop=True)
-    if save:
-        df.to_excel(save, index=False)
-    print(df.head(50))
+    return df
+    print(df)
 
-filterer(reasons, '/Users/hmurray/Desktop/Data_Briefs/ASE/Why_seek_business_advice/Data/mentor_reasons.xlsx')
-filterer(source, '/Users/hmurray/Desktop/Data_Briefs/ASE/Why_seek_business_advice/Data/mentor_sources.xlsx')
-filterer(outcome, '/Users/hmurray/Desktop/Data_Briefs/ASE/Why_seek_business_advice/Data/mentor_outcomes.xlsx')
+reasons = filterer(reasons)
+source = filterer(source)
+outcome = filterer(outcome)
+
+# unstack
+reasons['all_firms'] = reasons['percent'].iloc[0:15].reset_index(drop=True)
+reasons['less_2_years'] = reasons['percent'].iloc[15:30].reset_index(drop=True)
+reasons['Hispanic'] = reasons['percent'].iloc[30:45].reset_index(drop=True)
+reasons['White'] = reasons['percent'].iloc[45:60].reset_index(drop=True)
+reasons['Black'] = reasons['percent'].iloc[60:75].reset_index(drop=True)
+reasons['Asian'] = reasons['percent'].iloc[75:90].reset_index(drop=True)
+reasons.drop(reasons.index[15: ], inplace=True)
+print(reasons)
+
+sys.exit()
 
