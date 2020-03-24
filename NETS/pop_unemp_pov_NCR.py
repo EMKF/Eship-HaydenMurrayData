8f0a81f86a46f97bfb9f9edde03257fa7d4ca66b
htmurray15 <hmurray@MacMurH111919.lan1>
2020-03-24 14:15:55 -0500

    NCR

---
+++ NETS/pop_unemp_pov_NCR.py
@@ -0,0 +1,52 @@
+# pop data downloaded manually from: https://data.ers.usda.gov/reports.aspx?ID=17827
+# unemp data downloaded manually from: https://data.ers.usda.gov/reports.aspx?ID=17828
+# pov data downloaded manually from: https://data.ers.usda.gov/reports.aspx?ID=17826
+
+import os
+import sys
+import shutil
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import xlsxwriter
+from textwrap import wrap
+
+pd.set_option('max_columns', 1000)
+pd.set_option('max_info_columns', 1000)
+pd.set_option('expand_frame_repr', False)
+pd.set_option('display.max_rows', 30000)
+pd.set_option('max_colwidth', 4000)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+pd.options.mode.chained_assignment = None
+
+
+# pull sjc  and NEB
+pop = pd.read_excel('/Users/hmurray/Desktop/Data_Briefs/NETS/Danny_Smith_briefs/Four_Brief_Assignments/DC_Abnormality/python_edits/pop_unemp_pov_NCR/pop_unemp_pov_NCR.xlsx', sheet_name='Population')
+unemp = pd.read_excel('/Users/hmurray/Desktop/Data_Briefs/NETS/Danny_Smith_briefs/Four_Brief_Assignments/DC_Abnormality/python_edits/pop_unemp_pov_NCR/pop_unemp_pov_NCR.xlsx', sheet_name='Unemployment')
+pov = pd.read_excel('/Users/hmurray/Desktop/Data_Briefs/NETS/Danny_Smith_briefs/Four_Brief_Assignments/DC_Abnormality/python_edits/pop_unemp_pov_NCR/pop_unemp_pov_NCR.xlsx', sheet_name='Poverty')
+
+def _filter(df):
+    df = df[(df.County == 'District of Columbia') | (df.County == 'Montgomery County')| (df.County == 'Prince George\'s County') | (df.County == 'Arlington County') |\
+            (df.County == 'Fairfax County') | (df.County == 'Loudoun County') | (df.County == 'Prince William County')]
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+pop = _filter(pop)
+unemp = _filter(unemp)
+pov = _filter(pov)
+
+# rename unemp columns
+unemp.rename(columns={"2010": "Unemployment 2010", "2018": "Unemployment 2018"},inplace=True)
+pov.rename(columns={"Percent": "Percent in Poverty"},inplace=True)
+
+print(pop.head())
+print(unemp.head())
+print(pov.head())
+
+
+df = pd.concat([pop,unemp,pov],sort=True, axis=1)
+df = df[['County', 'FIPS*', 'Pop. 2010',  'Pop. 2018' , 'Change 2010-18', 'Unemployment 2010', 'Unemployment 2018', 'Median Household Income (2018)', '% of State Median HH Income', 'Percent in Poverty']]
+data = df.iloc[:, np.r_[2, 4:13]]
+print(data)
+data.to_excel('/Users/hmurray/Desktop/Data_Briefs/NETS/Danny_Smith_briefs/Four_Brief_Assignments/DC_Abnormality/python_edits/pop_unemp_pov_NCR/python_pop_unemp_pov_NCR.xlsx', index=False)
+sys.exit()
