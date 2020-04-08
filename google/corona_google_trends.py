028734778430b20fc1d1e8da94f84cba04e844df
htmurray15 <hmurray@MacMurH111919.lan1>
2020-04-08 15:48:00 -0500

    google2

--- google/corona_google_trends.py
+++ google/corona_google_trends.py
@@ -20,7 +20,7 @@ from pytrends.request import TrendReq
 
 pytrends = TrendReq(hl='en-US', tz=360)
 
-kw_list = ["Entrepreneurship"]
+kw_list = ["Entrepreneurship", "new business"]
 df = pytrends.get_historical_interest(kw_list, year_start=2018, month_start=1, day_start=1, hour_start=0, year_end=2018, month_end=2, day_end=1, hour_end=0, cat=0, geo='US', gprop='', sleep=0)
 print(df)
 
