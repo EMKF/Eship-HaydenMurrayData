0a48a46f637f8c3ea731d1a2bf26e9be2864b772
htmurray15 <hmurray@MacMurH111919.lan1>
2020-04-09 15:19:04 -0500

    BEA solved?

--- BEA/BEA_prop_inc.py
+++ BEA/BEA_prop_inc.py
@@ -1,3 +1,6 @@
+# data downloaded manually from tab, "Person Income (state and local)" option, "Annual Personal Income by State"
+# url: https://apps.bea.gov/regional/downloadzip.cfm
+
 import os
 import sys
 import shutil
@@ -23,30 +26,30 @@ inc = pd.read_csv('/Users/hmurray/Desktop/data/BEA/BEA_Data/BEA_income.csv')
 emp = emp[emp.columns[pd.Series(emp.columns).str.startswith(('GeoName', 'Description', '2'))]]
 inc = inc[inc.columns[pd.Series(inc.columns).str.startswith(('GeoName', 'Description', '2'))]]
 
+
+
+# FILTER FUNCTION
+def filterer(df):
+    df = df[df['GeoName'] == 'United States']
+    df.reset_index(inplace=True, drop=True)
+    return df
+emp = filterer(emp)
+inc = filterer(inc)
+
+# Filter for Proprietors employment and Proprietors' income 8/
+emp = emp[emp['Description'] == ' Proprietors employment']
+inc = inc[inc['Description'] == ' Proprietors\' income 8/']
+
+print(emp.head(100))
+print(inc.head(100))
+
+sys.exit()
+
 # convert to numeric
 df_inc['2018:Q1'] = pd.to_numeric(df_inc['2018:Q1'])
 
 # sum quarterly values of income
 # inc['2018'] = inc['2018:Q1'] + inc['2018:Q2'] + inc['2018:Q3'] + inc['2018:Q4']
-inc.groupby((np.arange(len(inc.columns)) // 4) + 1, axis=1).sum().add_prefix('s')
-
-print(emp.head(100))
-print(inc.head(100))
-sys.exit()
-
-# FILTER FUNCTION
-def filterer(df):
-    df = df[df['region'] == 'United States']
-    df = df[df['industry'] == 'Total for all sectors']
-    df = df[(df.demographic == 'All firms') | (df.demographic == 'Male')\
-            | (df.demographic == 'Female') | (df.demographic == 'Hispanic') | (df.demographic == 'White')\
-            | (df.demographic == 'Asian') | (df.demographic == 'Black or African American')]
-    df = df[(df.firm_age == 'All firms') | (df.firm_age == 'Firms with less than 2 years in business')]
-    df.reset_index(inplace=True, drop=True)
-    df.drop(df.columns[0:2], axis=1, inplace=True)
-    df.reset_index(inplace=True, drop=True)
-    return df
-reasons = filterer(reasons)
 
 # merge dataframes
 df_merge = pd.merge(df_emp, df_inc, on='GeoName')
