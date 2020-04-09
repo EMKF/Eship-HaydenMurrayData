e0db23aa95209babd047b940519336f11191a2f4
htmurray15 <hmurray@MacMurH111919.lan1>
2020-04-09 09:44:29 -0500

    google api methods

--- google/corona_google_trends.py
+++ google/corona_google_trends.py
@@ -1,3 +1,5 @@
+# pitrends code found here: https://pypi.org/project/pytrends/#interest-by-region
+
 import os
 import sys
 import shutil
@@ -6,6 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import xlsxwriter
 import pytrends
+from pytrends.request import TrendReq
 
 pd.set_option('max_columns', 1000)
 pd.set_option('max_info_columns', 1000)
@@ -16,11 +19,39 @@ pd.set_option('display.float_format', lambda x: '%.3f' % x)
 pd.options.mode.chained_assignment = None
 
 
-from pytrends.request import TrendReq
 
+# connect to google
 pytrends = TrendReq(hl='en-US', tz=360)
 
+
+
+
+# 1) build payload daily searches
 kw_list = ["Entrepreneurship", "new business"]
-df = pytrends.get_historical_interest(kw_list, year_start=2018, month_start=1, day_start=1, hour_start=0, year_end=2018, month_end=2, day_end=1, hour_end=0, cat=0, geo='US', gprop='', sleep=0)
+
+# .get_historical_interest
+df = pytrends.get_historical_interest(kw_list, year_start=2018, year_end=2018, cat=0, geo='US', sleep=0)
 print(df)
 
+
+
+# # 2) country searches for "start a business"
+# # build payload
+# pytrends.build_payload(kw_list=["start a business"])
+# # get .interest_by_region
+# df = pytrends.interest_by_region(resolution='COUNTRY', inc_low_vol=True, inc_geo_code=False)
+# print(df)
+# # plot
+# df.reset_index().plot(x="geoName", y="start a business", figsize=(120, 10), kind ="bar")
+
+
+
+# # 3) today's top searches
+# df = pytrends.trending_searches(pn="united_states")
+# print(df)
+
+
+
+# # 4) related searches
+# df = pytrends.related_topics("startup")
+# print(df)
