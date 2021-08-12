1c3a4bef2fea34997a1aaf0e68c9b398ee0533b0
htmurray15 <hmurray@kauffman.org>
2021-08-12 08:26:48 -0500

    scrape saver 8.12

--- web_scraping/web_scrape_tutorial.py
+++ web_scraping/web_scrape_tutorial.py
@@ -3,26 +3,38 @@
 # import Python's standard library for working with URLs
 from urllib.request import urlopen
 
-# define the URL we want to access and open it
-url = "http://olympus.realpython.org/profiles/aphrodite"
-page = urlopen(url)
+def aphrodite_web_scrape_tutorial1():
+    # define the URL we want to access and open it
+    url = "http://olympus.realpython.org/profiles/aphrodite"
+    page = urlopen(url)
+    # extract HTML from the webpage with .read() and then decode with .decode("utf-8")
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    # print the contents of the webpage
+    print(html)
+    # Method 1 for extracting text from HTML (NOTE: this gives us the index of the title tag, not the index of the title)
+    title_index = html.find("<title>")
+    print(title_index)
+    # use this to get the index of the first letter in the title
+    start_index = title_index + len("<title>")
+    print(start_index)
+    # use this to get the index of the closing </title> tag
+    end_index = html.find("</title>")
+    print(end_index)
+    # FINALLY, extract the title by slicing the HTML string
+    title = html[start_index:end_index]
+    print(title)
 
-# extract HTML from the webpage with .read() and then decode with .decode("utf-8")
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+def poseidon_web_scrape_tutorial2():
+    url = 'http://olympus.realpython.org/profiles/poseidon'
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    start_index = html.find("<title>") + len("<title>")
+    end_index = html.find("</title>")
+    title = html[start_index:end_index]
+    print(title)
 
-# print the contents of the webpage
-print(html)
 
-# Method 1 for extracting text from HTML (NOTE: this gives us the index of the title tag, not the index of the title)
-title_index = html.find("<title>")
-print(title_index)
-
-# use this to get the index of the first letter in the title
-start_index = title_index + len("<title>")
-print(start_index)
-
-# use this to get the index of the closing </title> tag
-end_index = html.find("</title>")
-print(end_index)
-
+if __name__ == '__main__':
+    aphrodite_web_scrape_tutorial1()
+    poseidon_web_scrape_tutorial2()
